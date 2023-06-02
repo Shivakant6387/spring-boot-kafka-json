7275af76517c8b233c50785f9ecdf31290fe07ee
Shivakant6387 <shivakant5875@gmail.com>
2023-06-02 13:42:59 +0530

    nothing to commit

---
+++ src/main/java/com/example/springbootkafkajson/SpringBootKafkaJsonApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootkafkajson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKafkaJsonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootKafkaJsonApplication.class, args);
+	}
+
+}
