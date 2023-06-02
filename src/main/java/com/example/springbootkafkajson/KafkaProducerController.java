5ba018086a5738add27938dfd8b6840bc34c91a5
Shivakant6387 <shivakant5875@gmail.com>
2023-06-02 14:02:09 +0530

    rest controller for producer

---
+++ src/main/java/com/example/springbootkafkajson/KafkaProducerController.java
@@ -0,0 +1,20 @@
+package com.example.springbootkafkajson;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/kafka")
+public class KafkaProducerController {
+    @Autowired
+    private KafkaProducer kafkaProducer;
+    @PostMapping("/publish")
+    public ResponseEntity<String>publish(@RequestBody User user){
+        kafkaProducer.sendMessage(user);
+        return ResponseEntity.ok("Message sent to kafka topic");
+    }
+}
