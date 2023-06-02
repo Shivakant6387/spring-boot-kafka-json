183402efd358d850abffa1f7ac58c5238d81027c
Shivakant6387 <shivakant5875@gmail.com>
2023-06-02 13:56:49 +0530

    producer

---
+++ src/main/java/com/example/springbootkafkajson/AppConstants.java
@@ -0,0 +1,6 @@
+package com.example.springbootkafkajson;
+
+public class AppConstants {
+    public static final String TOPIC_NAME = "json-topic";
+    public static final String GROUP_ID = "group_id";
+}
